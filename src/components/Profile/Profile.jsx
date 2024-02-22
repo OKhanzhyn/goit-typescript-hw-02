1d18d3935687e514cae96fbba4a7c63fea416d0e
OKhanzhyn <dev.khan.world@gmail.com>
2024-02-22 22:28:53 +0200

    HW01_01_css_module

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,31 +1,32 @@
 // rafce - сніпет
-import './ProfileStyles.css';
+import css from './Profile.module.css';
 
 const Profile = (props) => {
+  
   return ( 
-  <div className="profCard">
-  <div className='profCardUserData'>
-    <img className='profCardImg'
+  <div className={css.profCard}>
+  <div className={css.profCardUserData}>
+    <img className={css.profCardImg}
       src={props.image}
       alt="User avatar"
     />
-    <p className='profCardName'>{props.name}</p>
-    <p className='profcardTag'>@{props.tag}</p>
-    <p className='profCardLocation'>{props.location}</p>
+    <p className={css.profCardName}>{props.name}</p>
+    <p className={css.profcardTag}>@{props.tag}</p>
+    <p className={css.profCardLocation}>{props.location}</p>
   </div>
 
-  <ul className='profCardStats'>
-    <li className='profCardStatsItem'>
-      <span className='profCardStatsItemName'>Followers</span>
-      <span className='profCardStatsItemValue'>{props.stats.followers}</span>
+  <ul className={css.profCardStats}>
+    <li className={css.profCardStatsItem}>
+      <span className={css.profCardStatsItemName}>Followers</span>
+    <span className={css.profCardStatsItemValue}>{props.stats.followers}</span>
     </li>
-    <li className='profCardStatsItem'>
-      <span className='profCardStatsItemName'>Views</span>
-      <span className='profCardStatsItemValue'>{props.stats.views}</span>
+    <li className={css.profCardStatsItem}>
+      <span className={css.profCardStatsItemName}>Views</span>
+    <span className={css.profCardStatsItemValue}>{props.stats.views}</span>
     </li>
-    <li className='profCardStatsItem'>
-      <span className='profCardStatsItemName'>Likes</span>
-      <span className='profCardStatsItemValue'>{props.stats.likes}</span>
+    <li className={css.profCardStatsItem}>
+      <span className={css.profCardStatsItemName}>Likes</span>
+      <span className={css.profCardStatsItemValue}>{props.stats.likes}</span>
     </li>
   </ul>
   </div>
